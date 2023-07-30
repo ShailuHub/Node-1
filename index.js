a1dc673f0af0c4b91e23a633631da7d2cac1b21d
Shailu <shailesh.respond@gmail.com>
2023-07-30 17:12:45 +0530

    third

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Montee");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("</html>");
+  const url = req.url;
+  if (url === "/home") {
+    return res.write("<body><h1>Welcome home</h1></body>");
+  } else if (url === "/about") {
+    return res.write("<body><h1>Welcome to About Us page</h1></body>");
+  } else if (url === "/node") {
+    return res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  }
+  res.end();
 });
 
 server.listen(4000);
